54852e5aabba8c2b2769838ab020d0a54cea3497
pawanshettyy <pawanshetty13092006@gmail.com>
2024-10-31 15:51:03 +0530

    Added Feature : Live Cursors and Live Cursor Chat

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { Room } from "./Room";
+"use client";
 
+import Live from "@/components/Live";
 
 export default function Page() {
   return (
-    <div className="h-[100vh] w-full flex justify-center items-center text-center">
-      <h1 className="text-2xl text-white">Web Draw</h1>
+    <div>
+      <Live />
     </div> 
   );
 }
