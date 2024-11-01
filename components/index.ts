29977f80879278915e29bbffb55a13b27d4b1208
pawanshettyy <pawanshetty13092006@gmail.com>
2024-11-01 13:23:33 +0530

    Fixing Important Files

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,9 @@
 import LiveCursors from "./cursor/LiveCursors";
 import ReactionSelector from "./reaction/ReactionButton";
 import FlyingReaction from "./reaction/FlyingReaction";
-// import Navbar from "./Navbar";
-// import LeftSidebar from "./LeftSidebar";
-// import RightSidebar from "./RightSidebar";
+import Navbar from "./Navbar";
+import LeftSidebar from "./LeftSidebar";
+import RightSidebar from "./RightSidebar";
 import CursorChat from "./cursor/CursorChat";
 import Live from "./Live";
 
@@ -11,9 +11,9 @@ export {
   LiveCursors,
   ReactionSelector,
   FlyingReaction,
-//   Navbar,
-//   LeftSidebar,
-//   RightSidebar,
+  Navbar,
+  LeftSidebar,
+  RightSidebar,
   CursorChat,
   Live,
 };
