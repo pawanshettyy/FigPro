e96c7f9948c2dec7a8da363d0917dda6f716e764
pawanshettyy <pawanshetty13092006@gmail.com>
2024-11-02 11:56:06 +0530

    miscellaneous

--- app/Room.tsx
+++ app/Room.tsx
@@ -1,20 +1,41 @@
 "use client";
 
-import { ReactNode } from "react";
-import {
-  LiveblocksProvider,
-  RoomProvider,
-  ClientSideSuspense,
-} from "@liveblocks/react/suspense";
+import { LiveMap } from "@liveblocks/client";
+import { ClientSideSuspense } from "@liveblocks/react";
 
-export function Room({ children }: { children: ReactNode }) {
+import Loader from "@/components/Loader";
+import { RoomProvider } from "@/liveblocks.config";
+
+const Room = ({ children }: { children: React.ReactNode }) => {
   return (
-    <LiveblocksProvider publicApiKey={"pk_dev_yIN4Xfs0Nh4sEAegyPVG3otUQDjaoy8HUtATsDOKE1ZtDW8e_MPVIXMyP5mroWtC"}>
-      <RoomProvider id="my-room">
-        <ClientSideSuspense fallback={<div>Loading…</div>}>
-          {children}
-        </ClientSideSuspense>
-      </RoomProvider>
-    </LiveblocksProvider>
+    <RoomProvider
+      id="fig-room"
+      /**
+       * initialPresence is used to initialize the presence of the current
+       * user in the room.
+       *
+       * initialPresence: https://liveblocks.io/docs/api-reference/liveblocks-react#RoomProvider
+       */
+      initialPresence={{ cursor: null, cursorColor: null, editingText: null }}
+      /**
+       * initialStorage is used to initialize the storage of the room.
+       *
+       * initialStorage: https://liveblocks.io/docs/api-reference/liveblocks-react#RoomProvider
+       */
+      initialStorage={{
+        /**
+         * We're using a LiveMap to store the canvas objects
+         *
+         * LiveMap: https://liveblocks.io/docs/api-reference/liveblocks-client#LiveMap
+         */
+        canvasObjects: new LiveMap(),
+      }}
+    >
+      <ClientSideSuspense fallback={<Loader />}>
+        {() => children}
+      </ClientSideSuspense>
+    </RoomProvider>
   );
 }
+
+export default Room;
