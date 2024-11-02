791b9886b6124fb6cab2c7561fd2db70e3d3b422
pawanshettyy <pawanshetty13092006@gmail.com>
2024-11-02 11:54:21 +0530

    Fixed Project Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import { Work_Sans } from "next/font/google";
+
 import "./globals.css";
-import { Room } from "./Room";
-// import { TooltipProvider } from "@/components/ui/tooltip";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+import Room from "./Room";
 
 export const metadata = {
-  title: "Web Draw",
-  description: "A minimalist Figma clone using fabric.js and Liveblocks for realtime collaboration",
+  title: "FigPro by Pawan Shetty",
+  description:
+    "A minimalist Figma clone using fabric.js and Liveblocks for realtime collaboration",
 };
 
 const workSans = Work_Sans({
@@ -15,10 +18,10 @@ const workSans = Work_Sans({
 });
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
-  <html lang="en">
+  <html lang='en'>
     <body className={`${workSans.className} bg-primary-grey-200`}>
       <Room>
-          { children }
+        <TooltipProvider>{children}</TooltipProvider>
       </Room>
     </body>
   </html>
